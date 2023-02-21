3b5d7f5a6f2c93f1ef1326d9788e6ba93f1139d1
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-21 21:11:43 +0530

    create thread fun( )

--- App/main.c
+++ App/main.c
@@ -3,11 +3,22 @@
 #include<gst/gst.h>
 #include<pthread.h>
 
-
-int int argc, char* argv[])
+void* myThread(void *arg)
 {
+    while(1)
+    {
+        printf(" running.. \n");
+    }
   
+    return NULL;
+}
 
-  return 0;
+int main(int argc, char* argv[])
+{
+    pthread_t t_id; 
+    pthread_create(&t_id, NULL, myThread , NULL);
+    pthread_join(t_id,NULL);
+
+    return 0;
 
 }
