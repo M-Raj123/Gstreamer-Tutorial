2c97b94b225738eebde8a75b95407b7a3e23022a
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-23 17:31:07 +0530

    implement the controller thread

--- App/lib.c
+++ App/lib.c
@@ -47,3 +47,87 @@ void destroyResource(struct custom_data *data)
     gst_object_unref (data->pipeline);
 }
 
+/* controller thread */
+void * ctrlThread(void *arg)
+{
+
+    struct custom_data *ptr = (struct custom_data *) arg;
+
+    static int n,flag=0;
+    
+    pthread_t b_id;
+    
+    printf("inside the fun()\n");
+    
+    while(1)
+    {
+
+        printf("enter the choice:\n");
+        printf("1: file path:\n");
+        printf("2: play\n");
+        printf("3: pause\n");
+        printf("4: new path\n");
+        printf("5: close current video\n");
+        printf("6. exit\n");
+
+        scanf("%d",&n);
+        printf("num: %d \n",n );
+
+        if(n==6) 
+        {   
+            if(!flag) 
+            {
+                destroyResource(ptr);
+            }
+
+            pthread_cancel(b_id);
+            printf("exitt....\n");
+            break;
+        }
+
+        printf("before switch....\n");
+
+        switch(n)
+        {
+            case 1: 
+            {       printf("construct pipeline\n");
+                    toCreatePipeline(ptr);
+                    pthread_create(&b_id, NULL, busThread , ptr);
+                    break;
+            }
+            case 2: 
+            {
+                    printf("play\n");
+                    stateChangePlay(ptr);
+                    break;
+            }
+            case 3: 
+            {
+                    printf("pause\n");
+                    stateChangePause(ptr);
+                    break;
+            }
+            case 4: 
+            {
+                    printf("new path\n");
+                    toCreatePipeline(ptr);  
+                    break; 
+            }
+            case 5:
+            {
+                    printf("video close\n");
+                    destroyResource(ptr);
+                    flag=1;
+                    printf("exit ---\n");
+                    break;
+            } 
+            default: 
+            {
+                printf("exit switch\n");
+                break;
+            }
+        }
+
+    }
+    return NULL;
+}
