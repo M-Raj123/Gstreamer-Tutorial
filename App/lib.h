a5992a7b3f88e35351261583b274e05ba4cb195d
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-24 08:23:31 +0530

    Function Declarations

--- App/lib.h
+++ App/lib.h
@@ -6,3 +6,13 @@ struct custom_data {
     GstEvent *event;
     char path[200];
 };
+
+void toCreatePipeline(struct custom_data *data);
+void stateChangePause(struct custom_data *data);
+void stateChangePlay(struct custom_data *data);
+void stateChangeNull(struct custom_data *data);
+void destroyResource(struct custom_data *data);
+void stateChangeToPlayagain(struct custom_data *data);
+void msg(struct custom_data *data);
+void * ctrlThread(void * arg);
+void * busThread(void *arg);
