e7a087210b9bb8716bed57d539fadf687c4c14bc
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2023-02-24 08:26:01 +0530

    Update main.c

--- App/main.c
+++ App/main.c
@@ -2,23 +2,21 @@
 #include<unistd.h>
 #include<gst/gst.h>
 #include<pthread.h>
-
-void* myThread(void *arg)
-{
-    while(1)
-    {
-        printf(" running.. \n");
-    }
-  
-    return NULL;
-}
+#include "lib.h"
 
 int main(int argc, char* argv[])
 {
+
+    struct custom_data data; 
+    
+    /* Initialize GStreamer */
+    gst_init(&argc ,&argv);
+    printf("initiated\n");  
+
     pthread_t t_id; 
-    pthread_create(&t_id, NULL, myThread , NULL);
+    pthread_create(&t_id, NULL, ctrlThread , &data);
     pthread_join(t_id,NULL);
 
     return 0;
-
 }
+
